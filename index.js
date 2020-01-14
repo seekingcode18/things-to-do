11ca981f2a96e3671be90c52cd344554f9217d14
lawcia <lawrencianjume@gmail.com>
2020-01-14 11:29:47 +0000

    create schema

--- index.js
+++ index.js
@@ -1,14 +1,80 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
 const app = express();
 
 app.get('/', (req, res) => res.send('database homepage'))
+mongoose.connect('mongodb://localhost:27017/things_db', {
+  useNewUrlParser: true
+});
+const db = mongoose.connection;
 
-mongoose.connect('mongodb://localhost:27017/db', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('mongodb started');
-    app.listen(3000, () => {console.log('server started on port 3000')})
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  console.log('db running')
+  const activityShema = new mongoose.Schema({
+    category: {
+      type: String,
+      enum: ['Music', 'Galleries', 'Children', 'Food'],
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    location: {
+      street: {
+        type: String,
+        required: false
+      },
+      borough: {
+        type: String,
+        required: false
+      },
+      city: {
+        type: String,
+        required: true
+      },
+      postcode: {
+        type: String,
+        required: true
+      },
+      required: true
+    },
+    description: {
+      type: String
+    },
+    frequency: {
+      type: String,
+      enum: ['seasonal', 'ongoing', 'once']
+    },
+    start_date: {
+      type: Date,
+      required: function () {
+        if (this.frequency === 'seasonal') return true
+      }
+    },
+    end_date: {
+      type: Date,
+      required: function () {
+        if (this.frequency === 'seasonal') return true
+      }
+    },
+    date: {
+      type: Date
+    },
+    required: function () {
+      if (this.frequency === 'once') return true
+    },
+    cost: {
+      type: String,
+      enum: ['free', 'low', 'medium', 'high'],
+      required: true
+    },
   })
-  .catch((error) => console.log('mongodb connection failed. error: ', error));
+
+  const Activity = mongoose.model('Activity', activityShema);
+
+  
+app.listen(3000, () => console.log('server started on port 3000'))
+})
